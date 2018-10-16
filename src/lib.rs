1f6140ca3ac15770c61af857b9987cd0103e6255
NikVolf <nikvolf@gmail.com>
2018-10-16 14:54:07 +0300

    outer mul

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 extern crate num_complex;
+extern crate nalgebra;
 
 use std::ops::Mul;
 use num_complex::Complex64 as Complex;
+use nalgebra::Matrix2;
 
 pub struct Bra {
     a: Complex,
@@ -76,14 +78,31 @@ impl Ket {
 impl Mul<Ket> for Bra {
     type Output = Complex;
 
-    fn mul(self, other: Ket) -> Complex {
+    fn mul(self, other: Ket) -> Self::Output {
         self.a * other.a + self.b * other.b
+    }
+}
+
+impl Mul<Bra> for Ket {
+    type Output = Matrix2<Complex>;
+
+    fn mul(self, other: Bra) -> Self::Output {
+        let mut m = Matrix2::zeros();
+        unsafe {
+            *m.get_unchecked_mut(0, 0) = self.a * other.a;
+            *m.get_unchecked_mut(0, 1) = self.b * other.a;
+            *m.get_unchecked_mut(1, 0) = self.a * other.b;
+            *m.get_unchecked_mut(1, 1) = self.b * other.b;
+        }
+        m
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    const E: f64 = 0.00000000001;
 
     #[test]
     fn orthogonal() {
@@ -93,7 +112,14 @@ mod tests {
 
     #[test]
     fn prob() {
-        assert!((Bra::up() * Ket::left()).norm().powi(2) - 0.5 < 0.00000000001);
-        assert!((Bra::down() * Ket::right()).norm().powi(2) - 0.5 < 0.00000000001);
+        assert!((Bra::up() * Ket::left()).norm().powi(2) - 0.5 < E);
+        assert!((Bra::down() * Ket::right()).norm().powi(2) - 0.5 < E);
+    }
+
+    #[test]
+    fn outer() {
+        let outer = Ket::up() * Bra::down();
+        assert!(unsafe { outer.get_unchecked(0, 0).im } < E);
+        assert!(unsafe { outer.get_unchecked(0, 1).re } - 1.0 < E);
     }
 }
