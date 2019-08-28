8396643f2a5797ffe74ae5159086d50bfc948e29
Travis CI User <travis@example.org>
2019-08-28 12:34:04 +0000

    Update documentation

---
+++ implementors/std/panic/trait.UnwindSafe.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["brakets"] = [{text:"impl&lt;D&gt; !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.UnwindSafe.html\" title=\"trait std::panic::UnwindSafe\">UnwindSafe</a> for <a class=\"struct\" href=\"brakets/struct.Bra.html\" title=\"struct brakets::Bra\">Bra</a>&lt;D&gt;",synthetic:true,types:["brakets::bra::Bra"]},{text:"impl&lt;D&gt; !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.UnwindSafe.html\" title=\"trait std::panic::UnwindSafe\">UnwindSafe</a> for <a class=\"struct\" href=\"brakets/struct.Ket.html\" title=\"struct brakets::Ket\">Ket</a>&lt;D&gt;",synthetic:true,types:["brakets::ket::Ket"]},{text:"impl&lt;D&gt; !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.UnwindSafe.html\" title=\"trait std::panic::UnwindSafe\">UnwindSafe</a> for <a class=\"struct\" href=\"brakets/struct.Outer.html\" title=\"struct brakets::Outer\">Outer</a>&lt;D&gt;",synthetic:true,types:["brakets::outer::Outer"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["brakets"] = {"name":"","files":["bra.rs","ket.rs","lib.rs","outer.rs"]};
+createSourceSidebar();
